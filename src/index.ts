b4b999a9aadf5961f9f266e457f16eb29aa61f8c
Kyle Boas <kyleboas@gmail.com>
2026-03-01 12:12:31 +0800

    Delete old Sandbox DOs with v3 migration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import type { Env } from "./types";
 import { AgentDO } from "./do";
-import { Sandbox } from "./sandbox-do";
 import { getRepos, addRepo, getRepoGoals, setRepoGoals } from "./storage";
 import { Agent } from "./agent";
 import { handleSlackEvent } from "./slack";
@@ -73,4 +72,4 @@ export default {
   }
 };
 
-export { AgentDO, Sandbox };
+export { AgentDO };
